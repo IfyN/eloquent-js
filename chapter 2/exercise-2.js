e1225a5d596f1454c9196f82e6b911461d1ff1ba
IfyN <ifynwosu0@gmail.com>
2022-06-07 10:54:55 +0100

    fizzbuzz challenge solutions

--- chapter 2/exercise-2.js
+++ chapter 2/exercise-2.js
@@ -1,10 +1,26 @@
 //The Fizzbuzz challenge solution
 
-for (let number = 1; number <= 100; number ++){
-    result = "";
-   
-   if (number % 3 === 0) result += "Fizz" ;
-   if  (number % 5 === 0) result += "Buzz";
-    console.log (result || number);
- }
+//solution 1:
+for (let number = 1; number <= 100; number++) {
+  let byBoth = "";
 
+  if (number % 3 === 0) {
+    console.log("Fizz");
+  } else if (number % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(byBoth || number);
+  }
+}
+
+//solution 2:
+
+for (let number = 1; number <= 100; number++) {
+  result = "";
+
+  if (number % 3 === 0) result += "Fizz";
+  if (number % 5 === 0) result += "Buzz";
+  console.log(result || number);
+}
+
+//Both Output the same result
